a3a50773b546c9b0c3e7f03eea977f8f23f9a295
MattWellie <mattwellie@gmail.com>
2022-08-04 17:29:36 +1000

    massive updates with sample-metadata connection!

--- validation/validation_runner.py
+++ validation/validation_runner.py
@@ -4,13 +4,12 @@
 wraps the validation script(s)
 """
 
+import logging
 import os
-import sys
 from pathlib import Path
 from argparse import ArgumentParser
 
-import logging
-
+from cloudpathlib import AnyPath
 import hailtop.batch as hb
 from cpg_utils.config import get_config
 
@@ -27,16 +26,18 @@ from cpg_utils.hail_batch import (
     remote_tmpdir,
     image_path,
 )
+from sample_metadata.apis import AnalysisApi, ParticipantApi
+from sample_metadata.model.analysis_query_model import AnalysisQueryModel
+from sample_metadata.model.analysis_type import AnalysisType
 
 
-DEFAULT_IMAGE = get_config()["workflow"]["driver_image"]
-HAPPY_IMAGE = image_path("happy-vcfeval")
+DEFAULT_IMAGE = get_config()['workflow']['driver_image']
+HAPPY_IMAGE = image_path('happy-vcfeval')
 assert DEFAULT_IMAGE, HAPPY_IMAGE
 
-MT_TO_VCF_SCRIPT = os.path.join(os.path.dirname(__file__), "mt_to_vcf.py")
-QUERY_COMPARISON = os.path.join(os.path.dirname(__file__), "hail_query_validate.py")
-OUTPUT_VCFS = output_path("single_sample_vcfs")
-OUTPUT_VCF = output_path("variants_from_mt.vcf.bgz")
+MT_TO_VCF_SCRIPT = os.path.join(os.path.dirname(__file__), 'mt_to_vcf.py')
+QUERY_COMPARISON = os.path.join(os.path.dirname(__file__), 'hail_query_validate.py')
+OUTPUT_VCFS = output_path('single_sample_vcfs')
 
 # create a logger
 logger = logging.getLogger(__file__)
@@ -48,7 +49,7 @@ def set_job_resources(
     git=False,
     image: str | None = None,
     prior_job: hb.batch.job.Job | None = None,
-    memory: str = "standard",
+    memory: str = 'standard',
 ):
     """
     applied resources to the job
@@ -60,7 +61,7 @@ def set_job_resources(
     :param memory:
     """
     # apply all settings
-    job.cpu(2).image(image or DEFAULT_IMAGE).memory(memory).storage("20G")
+    job.cpu(2).image(image or DEFAULT_IMAGE).memory(memory).storage('20G')
 
     if prior_job is not None:
         job.depends_on(prior_job)
@@ -78,137 +79,165 @@ def set_job_resources(
         )
 
 
-def mt_to_vcf(batch: hb.Batch, input_file: str, header_lines: str | None):
+def mt_to_vcf(
+    batch: hb.Batch, input_file: str, header_lines: str | None, samples: list[str]
+):
     """
     takes a MT and converts to VCF
     adds in extra header lines for VQSR filters
     :param batch:
     :param input_file:
     :param header_lines:
+    :param samples:
     :return:
     """
-    mt_to_vcf_job = batch.new_job(name="Convert MT to VCF")
+    mt_to_vcf_job = batch.new_job(name='Convert MT to Single Sample VCFs')
 
     copy_common_env(mt_to_vcf_job)
 
     set_job_resources(mt_to_vcf_job, git=True, auth=True)
 
     job_cmd = (
-        f"PYTHONPATH=$(pwd) python3 {MT_TO_VCF_SCRIPT} "
-        f"--input {input_file} "
-        f"--output {OUTPUT_VCFS} "
+        f'PYTHONPATH=$(pwd) python3 {MT_TO_VCF_SCRIPT} '
+        f'--input {input_file} '
+        f'--output {OUTPUT_VCFS}'
+        f'--samples {" ".join(samples)} '
     )
 
     if header_lines:
-        job_cmd += f"--additional_header {header_lines}"
+        job_cmd += f'--additional_header {header_lines}'
 
-    logger.info(f"Command MT>VCF: {job_cmd}")
+    logger.info(f'Command MT>VCF: {job_cmd}')
 
     mt_to_vcf_job.command(job_cmd)
     return mt_to_vcf_job
 
 
 def comparison_job(
-    batch, sample, truth_vcf, bed: str, prior_job, annotate=False, combine=False
+    batch, ss_vcf: str, sample: str, truth_vcf: str, truth_bed: str, prior_job
 ):
     """
 
     Parameters
     ----------
     batch :
+    ss_vcf :
     sample :
     truth_vcf :
-    bed :
+    truth_bed :
     prior_job :
-    annotate :
-    combine :
 
     Returns
     -------
 
     """
-    if annotate and combine:
-        print("please pick a single setting [annotate/combine/split(default)]")
-        sys.exit(1)
 
-    vcf = os.path.join(OUTPUT_VCFS, f"{sample}.vcf.bgz")
-
-    job = batch.new_job(name=f"compare_{sample}")
+    job = batch.new_job(name=f'compare_{sample}')
     job.image(HAPPY_IMAGE)
-    job.memory("20Gi")
-    vcf_input = batch.read_input_group(**{"vcf": vcf, "index": vcf + ".tbi"})
+    job.memory('20Gi')
+    vcf_input = batch.read_input_group(**{'vcf': ss_vcf, 'index': ss_vcf + '.tbi'})
     truth_input = batch.read_input_group(
-        **{"vcf": truth_vcf, "index": truth_vcf + ".tbi"}
+        **{'vcf': truth_vcf, 'index': truth_vcf + '.tbi'}
     )
-    truth_bed = batch.read_input(bed)
+    truth_bed = batch.read_input(truth_bed)
     refgenome = batch.read_input(
-        "gs://cpg-reference/hg38/v0/dragen_reference/Homo_sapiens_assembly38_masked.fasta"
+        'gs://cpg-reference/hg38/v0/dragen_reference/'
+        'Homo_sapiens_assembly38_masked.fasta'
     )
 
-    if annotate:
-        job.declare_resource_group(
-            output={
-                "calls.vcf.gz": "{root}/calls.vcf.gz",
-                "calls.vcf.gz.tbi": "{root}/calls.vcf.gz.tbi",
-                "baseline.vcf.gz": "{root}/baseline.vcf.gz",
-                "baseline.vcf.gz.tbi": "{root}/baseline.vcf.gz.tbi",
-                "summary.txt": "{root}/summary.txt",
-            }
-        )
+    # hap.py outputs:
+    # output.extended.csv
+    # output.roc.all.csv.gz
+    # output.metrics.json.gz
+    # output.runinfo.json
+    # output.roc.Locations.INDEL.PASS.csv.gz
+    # output.summary.csv
+    # output.roc.Locations.INDEL.csv.gz
+    # output.vcf.gz
+    # output.roc.Locations.SNP.PASS.csv.gz
+    # output.vcf.gz.tbi
+    # output.roc.Locations.SNP.csv.gz
 
-    elif combine:
-        job.declare_resource_group(
-            output={
-                "combined_output.vcf.gz": "{root}/output.vcf.gz",
-                "combined_output.vcf.gz.tbi": "{root}/output.vcf.gz.tbi",
-                "summary.txt": "{root}/summary.txt",
-            }
-        )
-
-    else:
-        job.declare_resource_group(
-            output={
-                "fp.vcf.gz": "{root}/fp.vcf.gz",
-                "fp.vcf.gz.tbi": "{root}/fp.vcf.gz.tbi",
-                "fn.vcf.gz": "{root}/fn.vcf.gz",
-                "fn.vcf.gz.tbi": "{root}/fn.vcf.gz.tbi",
-                "tp.vcf.gz": "{root}/tp.vcf.gz",
-                "tp.vcf.gz.tbi": "{root}/tp.vcf.gz.tbi",
-                "summary.txt": "{root}/summary.txt",
-            }
-        )
-
-    # default mode is split
-    mode = ""
-    if annotate:
-        mode = "-m annotate "
-    if combine:
-        mode = "-m combine "
-
-    # in future don't regenerate SDF
-    job_cmd = (
-        f"java -jar -Xmx16G /vcfeval/RTG.jar format -o refgenome_sdf {refgenome} && "
-        f"mv {vcf_input['vcf']} input.vcf.gz && "
-        f"mv {vcf_input['index']} input.vcf.gz.tbi && "
-        f"java -jar -Xmx16G /vcfeval/RTG.jar vcfeval "
-        f"--decompose "
-        f"{mode}"
-        f"-t refgenome_sdf "
-        f"-b {truth_input['vcf']} "
-        f"-c input.vcf.gz "
-        f"-o {job.output} "
-        f"--bed-regions={truth_bed} && "
-        f"ls {job.output}"
+    job.declare_resource_group(
+        output={
+            'happy_extended.csv': '{root}/output.extended.csv',
+            'happy.vcf.gz': '{root}/output.vcf.gz',
+            'happy.vcf.gz.tbi': '{root}/output.vcf.gz.tbi',
+            'happy_roc.all.csv.gz': '{root}/output.roc.all.csv.gz',
+            'happy_metrics.json.gz': '{root}/output.metrics.json.gz',
+            'happy_runinfo.json': '{root}/output.runinfo.json',
+            'summary.csv': '{root}/output.summary.csv',
+        }
     )
 
-    job.command(job_cmd)
+    # in future don't regenerate SDF...
+    job.command(
+        f'java -jar -Xmx16G /vcfeval/RTG.jar format -o refgenome_sdf {refgenome} && '
+        f'mv {vcf_input["vcf"]} input.vcf.gz && '
+        f'mv {vcf_input["index"]} input.vcf.gz.tbi && '
+        f'mkdir {job.output} && '
+        f'hap.py {truth_input["vcf"]} input.vcf.gz '
+        f'-r {refgenome} -R {truth_bed} '
+        f'-o {job.output}/output --engine=vcfeval '
+        f'--engine-vcfeval-path=/vcfeval/rtg '
+        f'--engine-vcfeval-template refgenome.sdf '
+        f'--preprocess-truth'
+    )
     job.depends_on(prior_job)
-    batch.write_output(job.output, os.path.join(output_path("comparison"), sample))
-    return job
+    batch.write_output(job.output, os.path.join(output_path('comparison'), sample))
 
 
-def run_hail_benchmark(prior_job):
-    pass
+def get_validation_samples() -> dict[str, str]:
+    """
+    query metamist for all sample IDs
+    return a dict of {external: CPG ID}
+    Returns
+    -------
+
+    """
+
+    party = ParticipantApi()
+    results = party.get_external_participant_id_to_internal_sample_id(
+        project=get_config()['workflow']['dataset']
+    )
+    # pylint: disable=unnecessary-comprehension
+    return dict(results)
+
+
+def get_sample_truth(cpg_id: str) -> tuple[str, str]:
+    """
+    query metamist for the sample truth
+    Parameters
+    ----------
+    cpg_id :
+
+    Returns
+    -------
+    Path to the truth VCF and corresponding BED file
+
+    """
+    a_query_model = AnalysisQueryModel(
+        projects=[get_config()['workflow']['dataset']],
+        sample_ids=[cpg_id],
+        type=AnalysisType('custom'),
+        active=True,
+    )
+    anal_api = AnalysisApi()
+    analyses = anal_api.query_analyses(analysis_query_model=a_query_model)
+    if len(analyses) != 1:
+        logger.error(
+            f'Multiple [custom] analysis objects were found for '
+            f'{cpg_id}, please set old analyses to active=False'
+        )
+
+    analysis_meta = analyses[0]['meta']
+    truth_bed = analysis_meta.get('truth_bed')
+    truth_vcf = analysis_meta.get('truth_vcf')
+    assert (
+        truth_bed and truth_vcf
+    ), f'Missing one or both of the truth files: BED: {truth_bed}, VCF: {truth_vcf}'
+
+    return truth_bed, truth_vcf
 
 
 def main(input_file: str, header: str | None):
@@ -224,57 +253,59 @@ def main(input_file: str, header: str | None):
 
     """
 
+    validation_lookup = get_validation_samples()
+
     service_backend = hb.ServiceBackend(
-        billing_project=get_config()["hail"]["billing_project"],
+        billing_project=get_config()['hail']['billing_project'],
         remote_tmpdir=remote_tmpdir(),
     )
     batch = hb.Batch(
-        name="run validation bits and pieces",
+        name='run validation bits and pieces',
         backend=service_backend,
         cancel_after_n_failures=1,
         default_timeout=6000,
-        default_memory="highmem",
+        default_memory='highmem',
     )
 
     prior_job = None
 
     input_path = Path(input_file)
-    if input_path.suffix == ".mt":
+    if input_path.suffix == '.mt':
         # requires conversion to vcf
         prior_job = mt_to_vcf(
             batch=batch,
             input_file=input_file,
             header_lines=header,
+            samples=list(validation_lookup.keys()),
         )
 
-    twist_bed = "gs://cpg-validation-test/Twist_Exome_Core_Covered_Targets_hg38.bed"
-    # compare syndip
-    syndip_bed = "gs://cpg-reference/validation/syndip/regions/syndip.b38_20180222.bed"
-    _comparison_job = comparison_job(
-        batch=batch,
-        sample="syndip",
-        prior_job=prior_job,
-        bed=twist_bed,
-        truth_vcf="gs://cpg-validation-test/syndip/syndip_truth.vcf.gz",
-        combine=True,
-    )
-    # compare hg001
-    hg001_bed = "gs://cpg-validation-test/HG001/HG001_GRCh38_1_22_v4.2.1_benchmark.bed"
-    _comparison_job = comparison_job(
-        batch=batch,
-        sample="na12878_kccg",
-        prior_job=prior_job,
-        bed=twist_bed,
-        truth_vcf="gs://cpg-validation-test/HG001/HG001_GRCh38_1_22_v4.2.1_benchmark.vcf.gz",
-        combine=True,
-    )
+    single_sample_files = AnyPath(OUTPUT_VCFS).glob('*.vcf.bgz')
+
+    # for each sample, use metamist to pull the corresponding truth and VCF
+    # THEN GO AT IT BABY
+    # skip any samples without registered truth, complain
+    for ss_file in single_sample_files:
+        sample_id = ss_file.name.split('.vcf.bgz')[0]
+        cpg_id = validation_lookup[sample_id]
+        full_path = ss_file.absolute()
+        truth_bed, truth_vcf = get_sample_truth(cpg_id)
+        comparison_job(
+            batch=batch,
+            ss_vcf=full_path,
+            sample=sample_id,
+            prior_job=prior_job,
+            truth_bed=truth_bed,
+            truth_vcf=truth_vcf,
+        )
+
+    # twist_bed = 'gs://cpg-validation-test/Twist_Exome_Core_Covered_Targets_hg38.bed'
 
     batch.run(wait=False)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("-i", help="input_path")
-    parser.add_argument("--header", help="header_lines_file", default=None)
+    parser.add_argument('-i', help='input_path')
+    parser.add_argument('--header', help='header_lines_file', default=None)
     args = parser.parse_args()
     main(input_file=args.i, header=args.header)
