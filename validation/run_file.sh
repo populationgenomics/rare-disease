209925868123899740ee9a450b73b37aeaaeb9a2
MattWellie <mattwellie@gmail.com>
2022-09-30 18:04:14 +1000

    corrected path

--- validation/run_file.sh
+++ validation/run_file.sh
@@ -11,4 +11,4 @@ analysis-runner \
   --access-level test \
   validation/validation_runner.py \
     -i gs://cpg-validation-test/validation/copy/copy_of_exome_628.mt \
-    -s gs://cpg-validation-test/GRCh38_regions/twist/definition.tsv
+    -s gs://cpg-validation-test/GRCh38_regions/twist
