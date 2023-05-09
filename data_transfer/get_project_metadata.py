e2e46dd290b8022fa0a9fcffa667138b098fd964
EddieLF <eddieformaini@gmail.com>
2023-05-09 12:21:01 +1000

    use graphql, added tar all metadata files and upload to release

--- data_transfer/get_project_metadata.py
+++ data_transfer/get_project_metadata.py
@@ -1,10 +1,14 @@
 import csv
 import json
 import logging
+import os
+import subprocess
 import sys
+from datetime import datetime, timezone
 
 import click
 from sample_metadata.apis import FamilyApi, ParticipantApi, SampleApi
+from sample_metadata.graphql import query
 
 PAPI = ParticipantApi()
 SAPI = SampleApi()
@@ -32,12 +36,40 @@ def get_hpo_terms(individual_metadata: list[dict]):
 
 def get_pedigrees(project: str):
     """Get all pedigree rows for a project"""
-    return FAPI.get_pedigree(project)
+    _query = """
+             query ProjectPedigree($projectName: String!) {
+                 project(name: $projectName) {
+                     pedigree
+                 }
+             }
+             """
+
+    return query(_query, {'projectName': project}).get('project').get('pedigree')
 
 
 def get_sample_map(project: str):
     """Returns the internal id : external id sample map for a project"""
-    return SAPI.get_all_sample_id_map_by_internal(project)
+    _query = """
+            query ProjectSampleMap($projectName: String!) {
+                project(name: $projectName) {
+                    participants {
+                        samples {
+                            id
+                            externalId
+                        }
+                    }
+                }
+            }
+    """
+    samples_list = (
+        query(_query, {'projectName': project}).get('project').get('participants')
+    )
+
+    samples = [
+        samples.get('samples')[0] for samples in samples_list if samples.get('samples')
+    ]
+
+    return {sample.get('id'): sample.get('externalId') for sample in samples if sample}
 
 
 def write_outputs(
@@ -49,7 +81,7 @@ def write_outputs(
     """Writes the HPO terms file, pedigree file, and sample map file to the output path"""
 
     # HPO terms tsv
-    with open(f'{output_path}/hpo_terms.tsv', 'w') as f:
+    with open(f'{output_path}/hpo_terms.tsv', 'w', encoding='utf-8') as f:
         writer = csv.writer(f, delimiter='\t')
         for individual, hpo_terms in individual_hpo_terms.items():
             row = [individual, *list(hpo_terms)]
@@ -59,11 +91,11 @@ def write_outputs(
 
     # HPO terms json
     with open(f'{output_path}/hpo_terms.json', 'w') as f:
-        json.dump(individual_hpo_terms, f, indent=4)
+        json.dump(individual_hpo_terms, f, indent=4, sort_keys=True)
     logging.info(f'Wrote HPO terms json to {output_path}.')
 
     # Pedigree file
-    with open(f'{output_path}/pedigree.fam', 'w') as f:
+    with open(f'{output_path}/pedigree.fam', 'w', encoding='utf-8') as f:
         writer = csv.writer(f, delimiter='\t')
         writer.writerow(
             [
@@ -82,24 +114,58 @@ def write_outputs(
 
     # Sample map
     with open(f'{output_path}/external_translation.json', 'w') as f:
-        json.dump(sample_map, f, indent=4)
+        json.dump(sample_map, f, indent=4, sort_keys=True)
     logging.info(f'Wrote sample ID map json to {output_path}.')
+
+    subprocess.run(
+        ['tar', '-zcvf', 'metadata.tar.gz', f'{output_path}'],  # noqa: S603, S607
+        check=True,
+        stderr=subprocess.DEVNULL,
+    )
+    logging.info('Compressed metadata into metadata.tar.gz.')
+
+
+def upload_metadata_to_release(project: str):
+    """Uploads the compressed metadata .tar.gz into a directory with today's date in the release bucket"""
+    today = datetime.now(tz=timezone.utc).strftime('%Y-%m-%d')
+    release_path = f'gs://cpg-{project}-release/{today}/'
+
+    subprocess.run(
+        [  # noqa: S603, S607
+            'gcloud',
+            'storage',
+            '--billing-project',
+            project,
+            'cp',
+            './metadata.tar.gz',
+            release_path,
+        ],
+        check=True,
+    )
+    logging.info(f'Uploaded metadata.tar.gz to {release_path}')
 
 
 @click.command()
 @click.option('--project')
-@click.option('--output-path')
-def main(project: str, output_path: str):
+@click.option('--dry-run', is_flag=True)
+def main(project: str, dry_run: bool):
     """Creates the metadata files and saves them to the output path"""
+
+    output_path = f'{project}_metadata'
+    if not os.path.exists(f'./{output_path}'):
+        os.makedirs(f'./{output_path}')
+
     individual_metadata = get_individual_metadata(project)
     individual_hpo_terms = get_hpo_terms(individual_metadata)
 
     pedigrees = get_pedigrees(project)
-    print(type(pedigrees[0]))
 
     sample_map = get_sample_map(project)
 
     write_outputs(individual_hpo_terms, pedigrees, sample_map, output_path)
+
+    if not dry_run:
+        upload_metadata_to_release(project)
 
 
 if __name__ == '__main__':
