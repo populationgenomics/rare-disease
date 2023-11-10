109d08a15d2e7ded410f65400607f2fa3ab6d6d9
EddieLF <eddieformaini@gmail.com>
2023-11-10 16:45:35 +1100

    Remove gsutil usage and improve sequencing type iteration

--- data_management_scripts/cleanup_dataset_matrixtables.py
+++ data_management_scripts/cleanup_dataset_matrixtables.py
@@ -1,69 +1,81 @@
 #!/usr/bin/env python3  # noqa: EXE001
 
 import logging
+import os
 import subprocess
 import sys
 from datetime import datetime
 
 import click
+from cloudpathlib.anypath import to_anypath
 from cpg_utils.config import get_config
 from google.cloud import storage
 
 GENOME_PREFIX = 'mt/'
 EXOME_PREFIX = 'exome/mt/'
+MT_SUFFIXES = [GENOME_PREFIX, EXOME_PREFIX]
+SEQ_TYPE_MAP = {GENOME_PREFIX: 'genome', EXOME_PREFIX: 'exome'}
 
 
-def get_dataset_mt_timestamps(
-    dataset: str,
-    bucket: storage.Bucket,
-    prefix: str,
-):
-    """Finds the .mt directories and their create times in the seqr-main bucket"""
-    mt_timestamps: dict[str, datetime] | None = {}
-    try:
-        mt_folders = (
-            subprocess.run(  # Use subprocess because list_blobs lists everything inside each .mt folder
-                args=['gsutil', 'ls', f'gs://{bucket.name}/{prefix}'],
-                check=True,
-                capture_output=True,
-                text=True,
-            )
-            .stdout.strip()
-            .split('\n')
-        )
-    except subprocess.CalledProcessError:
-        logging.info(f'{dataset}:: No .mt folders found in {prefix}')
-        return mt_timestamps
+def get_dataset_mt_timestamps(dataset: str):
+    """
+    Finds the dataset .mt folders and their creation timestamps by
+    searching the /mt/ and /exome/mt/ paths for any .mt folders.
+    """
+    mt_folders_by_seq_type: dict[str, dict[str, datetime]] = {}
 
-    mt_folders = [
-        mt_folder.removeprefix(f'gs://{bucket.name}/') for mt_folder in mt_folders
-    ]
+    bucket_path = get_config()['storage'][dataset]['default']
+    bucket = storage.Client().get_bucket(bucket_path.removeprefix('gs://'))
 
-    for mt_folder in mt_folders:
-        logging.info(f'{dataset}:: Found: {mt_folder}')
-        blob = bucket.get_blob(mt_folder)
-        if not blob:  # If the .mt folder is not a blob, use the _SUCCESS file
-            blob = bucket.get_blob(f'{mt_folder}_SUCCESS')
-        mt_timestamps[mt_folder] = blob.time_created
+    for suffix in MT_SUFFIXES:
+        mt_timestamps: dict[str, datetime] | None = {}
 
-    return mt_timestamps
+        mt_root_folder = to_anypath(os.path.join(bucket_path, suffix))
+        if not mt_root_folder.exists():
+            continue
+
+        # Look inside the gs://cpg-dataset-main/mt/ and /exome/mt/ paths for .mt folders
+        mt_folders = list(mt_root_folder.iterdir())
+        mt_folders = [
+            str(mt_folder).removeprefix(f'{bucket_path}/') for mt_folder in mt_folders
+        ]
+
+        if any(not mt_folder.endswith('.mt/') for mt_folder in mt_folders):
+            raise ValueError(f'Invalid .mt folder: {mt_folders}')
+
+        for mt_folder in mt_folders:
+            blob = bucket.get_blob(mt_folder)
+            if not blob:  # If the .mt folder is not a blob, use the _SUCCESS file
+                blob = bucket.get_blob(f'{mt_folder}_SUCCESS')
+            try:
+                mt_timestamps[mt_folder] = blob.time_created
+            except AttributeError:
+                logging.warning(f'{dataset}:: Could not get timestamp for: {mt_folder}')
+                continue
+
+        if mt_timestamps:
+            mt_folders_by_seq_type[SEQ_TYPE_MAP[suffix]] = mt_timestamps
+
+    return mt_folders_by_seq_type
 
 
 def get_mt_folders_to_delete(
     dataset: str,
     mt_timestamps_dict: dict[str, datetime],
 ):
-    """Takes the list of .mt directories and keeps only the most recently created one"""
+    """Takes the list of .mt folders and keeps only the most recently created one"""
     dataset_mts = sorted(mt_timestamps_dict.items(), key=lambda x: x[1])
     dataset_mts_to_delete = [mt[0] for mt in dataset_mts[:-1]]
 
     logging.info(
-        f'{dataset}:: Keeping latest: "{dataset_mts[-1][0]}", created: {dataset_mts[-1][1].strftime("%Y-%m-%dT%H:%M:%S")}',
+        f'{dataset}:: Keeping latest: {dataset_mts[-1][0]}, created: {dataset_mts[-1][1].strftime("%Y-%m-%dT%H:%M:%S")}',
     )
 
     logging.info(
         f'{dataset}:: Found {len(dataset_mts_to_delete)} .mt entries to delete',
     )
+    for mt in dataset_mts_to_delete:
+        logging.info(mt)
     return dataset_mts_to_delete
 
 
@@ -71,10 +83,10 @@ def delete_dataset_mts(dataset: str, dataset_mts_to_delete: list[str]):
     """Takes a list of .mt folders and deletes them entirely"""
     subprocess.run(
         [  # noqa: S603, S607
-            'gsutil',
-            '-m',
+            'gcloud',
+            'storage',
             'rm',
-            '-r',
+            '--recursive',
             *dataset_mts_to_delete,
         ],
         check=True,
@@ -92,49 +104,22 @@ def delete_dataset_mts(dataset: str, dataset_mts_to_delete: list[str]):
 )
 def main(dry_run: bool, datasets: list[str]):
     """Run the seqr_load analyser and deleter for genomes and exomes"""
-    client = storage.Client()
-    config = get_config()
 
     for dataset in datasets:
-        bucket_name = config['storage'][dataset]['default'].removeprefix('gs://')
-        bucket = client.get_bucket(bucket_name)
-
-        logging.info(f'{dataset}:: GENOME')
-        genome_loads = get_dataset_mt_timestamps(
+        matrixtables_by_seqtype = get_dataset_mt_timestamps(
             dataset=dataset,
-            bucket=bucket,
-            prefix=GENOME_PREFIX,
         )
-
-        if genome_loads:
-            genome_loads_to_delete = get_mt_folders_to_delete(
+        for sequencing_type, matrixtables in matrixtables_by_seqtype.items():
+            logging.info(f'{dataset}:: {sequencing_type.upper()}')
+            loads_to_delete = get_mt_folders_to_delete(
                 dataset=dataset,
-                mt_timestamps_dict=genome_loads,
+                mt_timestamps_dict=matrixtables,
             )
 
             if not dry_run:
                 delete_dataset_mts(
                     dataset=dataset,
-                    dataset_mts_to_delete=genome_loads_to_delete,
-                )
-
-        logging.info(f'{dataset}:: EXOME')
-        exome_loads = get_dataset_mt_timestamps(
-            dataset=dataset,
-            bucket=bucket,
-            prefix=EXOME_PREFIX,
-        )
-
-        if exome_loads:
-            exome_loads_to_delete = get_mt_folders_to_delete(
-                dataset=dataset,
-                mt_timestamps_dict=exome_loads,
-            )
-
-            if not dry_run:
-                delete_dataset_mts(
-                    dataset=dataset,
-                    dataset_mts_to_delete=exome_loads_to_delete,
+                    dataset_mts_to_delete=loads_to_delete,
                 )
 
 
