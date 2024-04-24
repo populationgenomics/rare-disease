3fb80517e9b71f8d6684e38e924c4f7bc481a05a
EddieLF <34049565+EddieLF@users.noreply.github.com>
2024-04-24 13:51:50 +1000

    Update metadata dump (#36)
    * Add family GUID map to metadata dump
    
    * Fix missing arg
    
    * Add test option
    
    * Open jsons with to_path
    
    * Check paths exist
    
    * Check paths exist
    
    * Print family guid map
    
    * Fix typo
    
    * Reverse sg participant map
    
    * Separate exome and genome seqr guids
    
    * Remove debug logging
    
    * Linting
    
    * Use defaultdict to prevent sg_id loss
    
    * Update DatasetVCF meta filtering in GQL query
    
    * dict.get default return empty list

--- data_transfer/create_dataset_dump_for_release.py
+++ data_transfer/create_dataset_dump_for_release.py
@@ -2,10 +2,17 @@
 
 """
 This scripts extracts the HPO terms for all individuals in a dataset, as
-well as the pedigree and internal SG : external sample ID mapping. These are
+well as the pedigrees, SG_ID : external participant ID mapping, and the
+external family ID : seqr family GUID mapping. These are
 saved to files on disk before being zipped and uploaded into the release
 bucket of the dataset, in a directory containing today's date.
-The latest dataset-vcf type analyses for the dataset are also uploaded.
+The latest dataset-vcf type analyses for the dataset are also uploaded.\
+
+Requires two files, one for exome and one for genome, containing the mapping of
+SG_ID to seqr family GUID. Pass the path containing these files to the script
+using the --seqr-guid-file-path flag.
+The files should be named:
+{dataset}_exome_seqr_processed.json AND {dataset}_genome_seqr_processed.json
 """
 
 import csv
@@ -14,13 +21,15 @@ import logging
 import os
 import subprocess
 import sys
+from collections import defaultdict
 from datetime import datetime, timezone
 from zipfile import ZipFile
 
 import click
+from cpg_utils import to_path
 from google.cloud import storage
 from metamist.apis import ParticipantApi
-from metamist.graphql import query
+from metamist.graphql import gql, query
 
 PAPI = ParticipantApi()
 
@@ -49,35 +58,85 @@ def get_hpo_terms(individual_metadata: list[dict]):
 
 def get_pedigrees(dataset: str):
     """Get all pedigree rows for a dataset"""
-    _query = """
-             query DatasetPedigree($datasetName: String!) {
-                 project(name: $datasetName) {
-                     pedigree
-                 }
-             }
-             """
+    _query = gql(
+        """
+        query DatasetPedigree($datasetName: String!) {
+            project(name: $datasetName) {
+                pedigree
+            }
+        }
+        """,
+    )
 
     return query(_query, {'datasetName': dataset})['project']['pedigree']
 
 
+def get_sg_id_to_family_guid_map(dataset: str, seqr_metadata_file_path: str):
+    """Reads the json files in the bucket and returns a mapping of SG ID to family GUID"""
+    exome_family_guid_map = {}
+    exome_file_path = to_path(
+        f'{seqr_metadata_file_path}/{dataset}_exome_seqr_processed.json',
+    )
+    if exome_file_path.exists():
+        with exome_file_path.open() as f:
+            exome_family_guid_map = json.load(f)
+
+    genome_family_guid_map = {}
+    genome_file_path = to_path(
+        f'{seqr_metadata_file_path}/{dataset}_genome_seqr_processed.json',
+    )
+    with genome_file_path.open() as f:
+        genome_family_guid_map = json.load(f)
+
+    return {'exome': exome_family_guid_map, 'genome': genome_family_guid_map}
+
+
+def get_family_guid_map(
+    pedigrees: list[dict[str, str]],
+    sg_participant_map: dict[str, str],
+    sg_id_family_guid_maps: dict[str, dict[str, str]],
+):
+    """Returns a mapping of family ID to GUID"""
+    participant_sg_id_map = defaultdict(list)
+    for sg_id, participant in sg_participant_map.items():
+        participant_sg_id_map[participant].append(sg_id)
+
+    family_guid_map_exome = {}
+    family_guid_map_genome = {}
+    for row in pedigrees:
+        individual_id = row['individual_id']
+        sg_ids = participant_sg_id_map.get(individual_id, [])
+        for sg_id in sg_ids:
+            exome_family_guid = sg_id_family_guid_maps['exome'].get(sg_id)
+            genome_family_guid = sg_id_family_guid_maps['genome'].get(sg_id)
+            if exome_family_guid:
+                family_guid_map_exome[row['family_id']] = exome_family_guid
+            if genome_family_guid:
+                family_guid_map_genome[row['family_id']] = genome_family_guid
+
+    return {'exome': family_guid_map_exome, 'genome': family_guid_map_genome}
+
+
 def get_participant_sg_map(dataset: str):
     """Returns the internal SG id : external participant id mapping for a dataset."""
-    _query = """
-            query DatasetSampleMap($datasetName: String!) {
-                project(name: $datasetName) {
-                    participants {
+    _query = gql(
+        """
+        query DatasetSampleMap($datasetName: String!) {
+            project(name: $datasetName) {
+                participants {
+                    externalId
+                    samples {
+                        id
                         externalId
-                        samples {
+                        sequencingGroups {
                             id
-                            externalId
-                            sequencingGroups {
-                                id
-                            }
                         }
                     }
                 }
             }
-            """
+        }
+        """,
+    )
 
     participants = query(_query, {'datasetName': dataset})['project']['participants']
 
@@ -99,6 +158,7 @@ def write_outputs(
     individual_hpo_terms: dict[str, list],
     pedigrees: list[dict],
     sg_partitipant_map: dict[str, str],
+    family_guid_maps: dict[str, dict[str, str]],
     output_path: str,
 ):
     """Writes the two HPO terms files, pedigree file, and sample map file to a zip."""
@@ -140,12 +200,18 @@ def write_outputs(
         json.dump(sg_partitipant_map, f, indent=4, sort_keys=True)
     logging.info(f'Wrote SG ID : Participant external ID map json to {output_path}.')
 
+    # Family GUID maps
+    with open(f'{output_path}/family_guid_map.json', 'w') as f:
+        json.dump(family_guid_maps, f, indent=4, sort_keys=True)
+    logging.info(f'Wrote family GUID map jsons to {output_path}.')
+
     # Zip everything
     with ZipFile(f'{dataset}_metadata.zip', 'w') as z:
         z.write(f'{output_path}/hpo_terms.tsv')
         z.write(f'{output_path}/hpo_terms.json')
         z.write(f'{output_path}/pedigree.fam')
         z.write(f'{output_path}/external_translation.json')
+        z.write(f'{output_path}/family_guid_map.json')
 
 
 def upload_metadata_to_release(dataset: str, billing_project: str | None):
@@ -193,7 +259,7 @@ def copy_vcf_to_release(dataset: str, billing_project: str | None):
     vcf_analyses = [
         analysis
         for analysis in analyses
-        if ('type', 'dataset-vcf') in analysis['meta'].items()
+        if ('stage', 'DatasetVCF') in analysis['meta'].items()
     ]
 
     if not vcf_analyses:
@@ -308,8 +374,15 @@ def copy_vcf_to_release(dataset: str, billing_project: str | None):
 @click.option('--dataset')
 @click.option('--billing-project', default=None)
 @click.option('--metadata-only', is_flag=True)
+@click.option('--seqr-metadata-file-path', '-s', required=True)
 @click.option('--dry-run', is_flag=True)
-def main(dataset: str, billing_project: str | None, metadata_only: bool, dry_run: bool):
+def main(
+    dataset: str,
+    billing_project: str | None,
+    metadata_only: bool,
+    seqr_metadata_file_path: str,
+    dry_run: bool,
+):
     """Creates the metadata files and saves them to the output path"""
 
     output_path = f'{dataset}_metadata'
@@ -323,11 +396,23 @@ def main(dataset: str, billing_project: str | None, metadata_only: bool, dry_run
 
     sg_participant_map = get_participant_sg_map(dataset)
 
+    sg_id_family_guid_maps = get_sg_id_to_family_guid_map(
+        dataset,
+        seqr_metadata_file_path,
+    )
+
+    family_guid_maps = get_family_guid_map(
+        pedigrees,
+        sg_participant_map,
+        sg_id_family_guid_maps,
+    )
+
     write_outputs(
         dataset,
         individual_hpo_terms,
         pedigrees,
         sg_participant_map,
+        family_guid_maps,
         output_path,
     )
 
