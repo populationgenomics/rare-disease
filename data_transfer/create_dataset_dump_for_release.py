983808da501348e035529ea60ec69a948da0d74a
EddieLF <34049565+EddieLF@users.noreply.github.com>
2025-07-21 14:44:49 +1000

    Add new stage name into DatasetVCF filter (#41)
    * Add new stage name into DatasetVCF filter
    
    * Lint
    
    * Update data_transfer/create_dataset_dump_for_release.py
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * Lint
    
    ---------
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>

--- data_transfer/create_dataset_dump_for_release.py
+++ data_transfer/create_dataset_dump_for_release.py
@@ -259,7 +259,8 @@ def copy_vcf_to_release(dataset: str, billing_project: str | None):
     vcf_analyses = [
         analysis
         for analysis in analyses
-        if ('stage', 'DatasetVCF') in analysis['meta'].items()
+        if analysis['meta'].get('stage')
+        in ['DatasetVCF', 'AnnotatedDatasetMtToVcfWithHailQuery']
     ]
 
     if not vcf_analyses:
