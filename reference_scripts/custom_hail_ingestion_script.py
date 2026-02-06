cbe47f60737c2bccffaa254a16e4eb18d00bfc1f
Johnnyassaf <johnny.assaf@populationgenomics.org.au>
2026-02-06 15:08:42 +1100

    linting

--- reference_scripts/custom_hail_ingestion_script.py
+++ reference_scripts/custom_hail_ingestion_script.py
@@ -1,22 +1,41 @@
-from cpg_utils import hail_batch
-import hail as hl
 import argparse
+
+import hail as hl
+from cpg_utils import hail_batch
 
 hail_batch.init_batch()
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(description='Import TSV file into Hail Table format')
-parser.add_argument('input_file', type=str, help='Path to the input TSV file (can be gzipped)')
-parser.add_argument('--output', '-o', type=str, default=None,
-                    help='Output path for Hail table (default: input_file.ht)')
-parser.add_argument('--reference-genome', '-r', type=str, default='GRCh38',
-                    choices=['GRCh37', 'GRCh38'],
-                    help='Reference genome to use (default: GRCh38)')
+parser.add_argument(
+    'input_file',
+    type=str,
+    help='Path to the input TSV file (can be gzipped)',
+)
+parser.add_argument(
+    '--output',
+    '-o',
+    type=str,
+    default=None,
+    help='Output path for Hail table (default: input_file.ht)',
+)
+parser.add_argument(
+    '--reference-genome',
+    '-r',
+    type=str,
+    default='GRCh38',
+    choices=['GRCh37', 'GRCh38'],
+    help='Reference genome to use (default: GRCh38)',
+)
 args = parser.parse_args()
 
 # Define paths
 tsv_path = args.input_file
-output_path = args.output if args.output else tsv_path.replace('.tsv.gz', '.ht').replace('.tsv', '.ht')
+output_path = (
+    args.output
+    if args.output
+    else tsv_path.replace('.tsv.gz', '.ht').replace('.tsv', '.ht')
+)
 
 # 1. Define the input types for the initial table import
 # We import chrom/pos/ref/alt as strings/ints first to transform them
@@ -25,22 +44,17 @@ input_types = {
     'POS': hl.tint32,
     'REF': hl.tstr,
     'ALT': hl.tstr,
-    'avis': hl.tfloat64
+    'avis': hl.tfloat64,
 }
 
 # 2. Load the TSV
-ht = hl.import_table(
-    tsv_path,
-    types=input_types,
-    delimiter='\t',
-    force_bgz=True
-)
+ht = hl.import_table(tsv_path, types=input_types, delimiter='\t', force_bgz=True)
 
 # 3. Transform to standard Hail genomic format
 # We create a 'locus' object and an 'alleles' array
 ht = ht.transmute(
     locus=hl.locus(ht['#CHROM'], ht.POS, reference_genome=args.reference_genome),
-    alleles=[ht.REF, ht.ALT]
+    alleles=[ht.REF, ht.ALT],
 )
 
 # 4. Key the table by locus and alleles
--- reference_scripts/custom_merge_script.py
+++ reference_scripts/custom_merge_script.py
@@ -1,21 +1,29 @@
-from cpg_utils import hail_batch
-import hail as hl
 import argparse
+
+import hail as hl
+from cpg_utils import hail_batch
+
 # 1. Initialize Hail with the Service Backend for Batch
 # This assumes you have your environment variables/billing project configured
 hail_batch.init_batch()
 parser = argparse.ArgumentParser(description='Import and merge Hail Tables')
 parser.add_argument('--input_file1', type=str, help='Path to the input hail table')
 parser.add_argument('--input_file2', type=str, help='Path to the input hail table')
-parser.add_argument('--output', '-o', type=str, default=None,
-                    help='Output path for merged Hail Table (default: merged.ht)')
+parser.add_argument(
+    '--output',
+    '-o',
+    type=str,
+    default=None,
+    help='Output path for merged Hail Table (default: merged.ht)',
+)
 
 args = parser.parse_args()
 HT_A = args.input_file1
 HT_B = args.input_file2
 DESTINATION = args.output if args.output else 'merged.ht'
 
-def merge_hail_tables(path_a, path_b, output_path):
+
+def merge_hail_tables(path_a: str, path_b: str, output_path: str) -> None:
     # Load the two Hail Tables
     ht1 = hl.read_table(path_a)
     ht2 = hl.read_table(path_b)
