cd959786effca8a40e87373f19b52680b6823fca
MattWellie <mattwellie@gmail.com>
2026-02-11 15:51:34 +1000

    set -eux

--- copy_phenio.sh
+++ copy_phenio.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eux
+
 curl -o phenio.db.gz https://data.monarchinitiative.org/monarch-kg/latest/phenio.db.gz
 gunzip phenio.db.gz
 gcloud storage cp phenio.db gs://cpg-common-test/references/talos/phenio.db
