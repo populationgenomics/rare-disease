e37880474fefaafc78092c37a1e26ca7c5d338a7
SamBryen <samantha.bryen@populationgenomics.org.au>
2026-03-17 18:07:22 +1100

    appeasing the linter gods and lowering the max AF values

--- investigations/mt_reformatting.py
+++ investigations/mt_reformatting.py
@@ -21,7 +21,6 @@ https://hail.is/docs/0.2/expressions.html
 from argparse import ArgumentParser
 
 import hail as hl
-
 from cpg_utils.hail_batch import init_batch
 
 
@@ -41,7 +40,7 @@ def main(input_path: str, output_path: str) -> None:
 
     # start a Query-on-batch runtime
     init_batch(
-        worker_cores=8, 
+        worker_cores=8,
         worker_memory='highmem',
         driver_cores=8,
         driver_memory='highmem',
@@ -51,9 +50,7 @@ def main(input_path: str, output_path: str) -> None:
     mt = load_in_mt(input_path)
 
     # 1. Count exactly how many rows are missing the 'avis' score
-    missing_avis_count = mt.aggregate_rows(
-        hl.agg.count_where(hl.is_missing(mt.avis))
-    )
+    missing_avis_count = mt.aggregate_rows(hl.agg.count_where(hl.is_missing(mt.avis)))
     print(f"Number of variants missing an AVI score: {missing_avis_count}")
 
     # 2. (Optional) Visually inspect what those missing rows look like
@@ -93,84 +90,62 @@ def main(input_path: str, output_path: str) -> None:
     # keep previously aggregated transcript consequence field https://github.com/populationgenomics/cpg-flow-seqr-loader/blob/main/src/cpg_seqr_loader/scripts/annotate_cohort.py#L267
     fields_to_keep.append('transcriptConsequenceTerms')
 
-
     # keep the max AlphaMissense score for all transcript consequences.
     mt = mt.annotate_rows(
         am_max_score=hl.or_missing(
             hl.is_defined(mt.vep.transcript_consequences),
             hl.max(
-                mt.vep.transcript_consequences
-                .filter(lambda tc: hl.is_defined(tc))
-                .map(lambda tc: tc.am_pathogenicity)
-            )
-        )
+                mt.vep.transcript_consequences.filter(lambda tc: hl.is_defined(tc)).map(
+                    lambda tc: tc.am_pathogenicity,
+                ),
+            ),
+        ),
     )
     fields_to_keep.append('am_max_score')
-    
+
     # UTR annotations will be tricky as they are applied per transcript, and you may want to keep both the transcript and result?
     # if you want to keep just the list of 5'UTR predicted consequences you can use some aggregation logic similar to above
     mt = mt.annotate_rows(
         utr_5_consequences=hl.or_missing(
             hl.is_defined(mt.vep.transcript_consequences),
             hl.set(
-                mt.vep.transcript_consequences
-                .filter(lambda tc: hl.is_defined(tc))
+                mt.vep.transcript_consequences.filter(lambda tc: hl.is_defined(tc))
                 .filter(lambda tc: hl.is_defined(tc['5utr_consequence']))
-                .map(lambda tc: tc['5utr_consequence'])
-            )
-        )
+                .map(lambda tc: tc['5utr_consequence']),
+            ),
+        ),
     )
     fields_to_keep.append('utr_5_consequences')
 
-	# keep AVI scores
+    # keep AVI scores
     fields_to_keep.append('avis')
 
     # remove all entries (genotypes) from the dataset where the sample was WT/HomRef
     mt = mt.filter_entries(mt.GT.is_hom_ref(), keep=False)
 
     # remove variants with low GQ
-    mt = mt.filter_entries(mt.GQ > 20)
+    min_gq = 20
+    mt = mt.filter_entries(min_gq < mt.GQ)
 
     # remove variants that didn't pass VQSR filters. Variants that 'PASS' are left blank.
     mt = mt.filter_rows(hl.len(mt.filters) == 0)
 
     # remove common variants
-    mt = mt.filter_rows(mt.gnomad_genomes.FAF_AF < 0.1)
-    mt = mt.filter_rows(mt.gnomad_exomes.FAF_AF < 0.1)
+    max_af = 0.01
+    mt = mt.filter_rows(max_af > mt.gnomad_genomes.FAF_AF)
+    mt = mt.filter_rows(max_af > mt.gnomad_exomes.FAF_AF)
 
-	# remove variants with high AC
-    mt = mt.filter_rows(
-        hl.or_missing(
-            hl.len(mt.info.AC) > 0, 
-            mt.info.AC[0]
-        ) < 50
-    )
+    # remove variants with high AC
+    max_ac = 50
+    mt = mt.filter_rows(hl.or_missing(hl.len(mt.info.AC) > 0, mt.info.AC[0]) < max_ac)
     # aggregate all sample IDs remaining
     # this would create a new field, `var_samples`, which is a set of all CPG IDs with variants fitting above criteria
     mt = mt.annotate_rows(
-        var_samples=hl.agg.filter(mt.GT.is_non_ref(), hl.agg.collect_as_set(mt.s))
+        var_samples=hl.agg.filter(mt.GT.is_non_ref(), hl.agg.collect_as_set(mt.s)),
     )
     fields_to_keep.append('var_samples')
 
-    # it probably makes sense to keep all genotypes fitting your strict criteria here
-    # later when you want to make specific choices, such as 'only affected', or 'only with RNA data'
-    # you can add a list of samples, make it into a hail object, and do some matches, e.g.
-    # rna_samples = ['cpg1', 'cpg3', ]
-    ## turn the variable into an expression (hail likes expressions...)
-    # hl_samples = hl.literal(rna_samples)
-
-    ## keep all rows where there's a sample with matched RNA
-    # ht.filter(hl.len(hl_samples.intersection(ht.var_samples)) > 0)
-
-    ## remove rows unless they're only related to affected participants
-    # affected_sams = [...]
-    # ht.filter(ht.var_samples.is_subset(affected_sams))
-
-    # there's much more comprehensive logic for entries here: https://github.com/populationgenomics/cpg-flow-seqr-loader/blob/main/src/cpg_seqr_loader/scripts/annotate_dataset.py#L13
-    # annotate_dataset generates a number of columns which retains more sample level information, e.g. sample GQ -> samples_gq.5_to_10, samples_gq.10_to_15, samples_gq.15_to_20
-    # this might be useful, it might be overkill - you might want to duplicate some of that into this script
-    
-	# adding checkpoint for stability
+    # adding checkpoint for stability
     checkpoint_path = output_path.replace('.ht', '_checkpoint.mt')
     mt = mt.checkpoint(checkpoint_path, overwrite=True)
 
@@ -180,7 +155,7 @@ def main(input_path: str, output_path: str) -> None:
     ht = ht.select(*fields_to_keep)
 
     # write this skinny HT to a new location
-    ht.show(5) #test to make sure logic works
+    ht.show(5)  # test to make sure logic works
     ht.write(output_path, overwrite=True)
 
     # print the number of rows
