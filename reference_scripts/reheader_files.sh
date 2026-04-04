b09495f532fc5f66235ee83ede003ef61e2e5f2d
Matt Welland <mattwellie@gmail.com>
2026-04-04 13:56:37 +1000

    Reheader AVI files (#51)
    * bash script for streamed reheading & bgzip compression, specific to AVI-PHRED column

---
+++ reference_scripts/reheader_files.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+IN=$1
+OUT=$2
+
+
+set -euxo pipefail
+gcloud storage cat "${IN}" | gunzip | awk '{if(NR==1){print "#CHROM\tPOS\tREF\tALT\tavis\tphred"}else{print $0}}' | bgzip -c | gcloud storage cp - "${OUT}"
